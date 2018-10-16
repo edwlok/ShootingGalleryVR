edb01c62b4c32e4d160ed7673eeb3edea389027b
[Edward Lok] <elok1@uci.edu>
2018-10-16 15:51:49 -0700

    imported working shooting need to implement targets

---
+++ OculusGoTest/Assets/Scripts/MoveUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MoveUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger)){
+            this.transform.Translate(Vector3.up * 5, Space.World);
+        }
+	}
+}
---
+++ OculusGoTest/Assets/Scripts/PistolShooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolShooting : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform spawnPos;
+    public float force = 30f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, spawnPos.position, spawnPos.rotation);
+
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * force);
+
+            Destroy(bullet, 3f);
+        }
+	}
+}
